585d5490f78f518ec93bf704ddac0c11d9a048b4
Pond <pondwachi.th@gmail.com>
2024-06-05 14:50:32 +0700

    feat: form of pond

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
+
 function ProductForm() {
+
+  const [name, setName] = useState('') 
+  const [image, setImage] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleSubmit = (e) => {
+  
+    const product= {
+      name,
+      price: Number(price),
+      image,
+      description
+    };
+
+    alert(JSON.stringify(product,null,1))
+  };
+
+
+
+
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
+   
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -9,8 +35,9 @@ function ProductForm() {
             id="name"
             name="name"
             type="text"
+            value={name}
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(e) => setName(e.target.value)}
           />
         </label>
       </div>
@@ -21,8 +48,9 @@ function ProductForm() {
             id="image"
             name="image"
             type="text"
+            value={image}
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(e) => setImage(e.target.value)}
           />
         </label>
       </div>
@@ -33,8 +61,9 @@ function ProductForm() {
             id="price"
             name="price"
             type="number"
+            value={price}
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(e) => setPrice(e.target.value)}
           />
         </label>
       </div>
@@ -45,15 +74,16 @@ function ProductForm() {
             id="description"
             name="description"
             type="text"
+            value={description}
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(e) => setDescription(e.target.value)}
             rows={4}
             cols={30}
           />
         </label>
       </div>
       <div className="form-actions">
-        <button type="submit">Create</button>
+        <button type="submit" >Create</button>
       </div>
     </form>
   );
